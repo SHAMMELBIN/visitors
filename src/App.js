294d586969acd36ebed63c13e9216a57696c6ed3
sham Melbin <shammelbin@gmail.com>
2025-11-19 18:34:24 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Visitors from './Components/Visitors';
+
+function App() {
+  return (
+    <div className="App">
+      <Visitors />
+    </div>
+  );
+}
+
+export default App;

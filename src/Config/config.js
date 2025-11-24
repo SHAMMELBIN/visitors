972ae439b9aff7ba368c5406fde72b99036b347f
sham Melbin <shammelbin@gmail.com>
2025-11-24 11:28:17 +0530

    pushing url

--- src/Config/config.js
+++ src/Config/config.js
@@ -1,28 +1,9 @@
 import axios from "axios";
 
-export const IMAGE_BASE_URL = "https://api.gripforum.com/api/public/";
-export const BASE_URL = " http://localhost:4002/api/mobile";
+export const IMAGE_BASE_URL = "https://api.gripforum.comy/api/public/";
+export const BASE_URL = "https://api.gripforum.com/api/mobile";
 export const server = "https://api.gripforum.com/"
 
-// muges wifi
-// export const IMAGE_BASE_URL = "http://192.168.230.114:3004/api/public";
-// export const BASE_URL = "http://192.168.230.114:3004/api/mobile";
-// export const server = "http://192.168.230.114:3004"
-
-// kesavan wifi
-// export const IMAGE_BASE_URL = "http://192.168.170.114:3004/api/public";
-// export const BASE_URL = "http://192.168.170.114:3004/api/mobile";
-// export const server = "http://192.168.170.114:3004"
-
-// office wifi
-// export const IMAGE_BASE_URL = "http://192.168.29.44:3004/api/public";
-// export const BASE_URL = "http://192.168.29.44:3004/api/mobile";
-// export const server = "http://192.168.29.44:3004"
-
-
-// export const IMAGE_BASE_URL = "http://localhost:3004/api/public";
-// export const BASE_URL = "http://localhost:3004/api/mobile";
-// export const server = "http://localhost:3004"
 
 export const apiClient = axios.create({
   baseURL: BASE_URL,
